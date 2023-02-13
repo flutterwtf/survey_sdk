0662c2c15238758164a5958443edc7924e798dbf
Alexander <casha4367@mail.ru>
2023-02-13 09:05:47 +0300

    add-choice-question/organize classes

--- admin/lib/presentation/pages/choice_question/choice_question_page.dart
+++ admin/lib/presentation/pages/choice_question/choice_question_page.dart
@@ -131,3 +131,97 @@ class _ChoiceQuestionPageState extends State<ChoiceQuestionPage>
     );
   }
 }
+
+class _QuestionCheckboxes extends StatelessWidget {
+  const _QuestionCheckboxes({
+    required this.options,
+    required this.onChanged,
+    required this.activeColor,
+    required this.inactiveColor,
+  });
+
+  final List<String> options;
+  final void Function(List<String>? selectedItems) onChanged;
+  final Color activeColor;
+  final Color inactiveColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        unselectedWidgetColor: inactiveColor,
+      ),
+      child: FormBuilder(
+        child: FormBuilderCheckboxGroup(
+          orientation: OptionsOrientation.vertical,
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+          ),
+          activeColor: activeColor,
+          onChanged: onChanged,
+          name: '',
+          options: options
+              .map(
+                (option) => FormBuilderFieldOption(
+                  value: option,
+                  child: Text(
+                    option,
+                    style: const TextStyle(
+                      fontSize: AppFonts.sizeM,
+                    ),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class _QuestionRadioButtons extends StatelessWidget {
+  const _QuestionRadioButtons({
+    required this.options,
+    required this.onChanged,
+    required this.activeColor,
+    required this.inactiveColor,
+  });
+
+  final List<String> options;
+  final void Function(String? selectedItem) onChanged;
+  final Color activeColor;
+  final Color inactiveColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        unselectedWidgetColor: inactiveColor,
+      ),
+      child: FormBuilder(
+        child: FormBuilderRadioGroup(
+          orientation: OptionsOrientation.vertical,
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+          ),
+          activeColor: activeColor,
+          onChanged: onChanged,
+          name: '',
+          options: options
+              .map(
+                (option) => FormBuilderFieldOption(
+                  value: option,
+                  child: Text(
+                    option,
+                    style: const TextStyle(
+                      fontSize: AppFonts.sizeM,
+                    ),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
