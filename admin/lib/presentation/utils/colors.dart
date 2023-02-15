281fc8fd89839ee0978b2525a055a0c3e296b7c9
Katya_Sobolevskaya <katyasobolevskaya11@gmail.com>
2023-02-15 20:45:31 +0300

    option-customize-item/ add option customize item

--- admin/lib/presentation/utils/colors.dart
+++ admin/lib/presentation/utils/colors.dart
@@ -11,5 +11,9 @@ abstract class AppColors {
   static const accentGreen = Color(0xFF42E884);
 
   static const text = Color(0xFF333333);
+  static const textLightGrey = Color(0xFFCCCCCC);
   static const textGrey = Color(0xFF292929);
+
+  static const iconGrey = Color(0xFF929292);
+
 }
---
+++ admin/lib/presentation/widgets/option_customize_item.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:survey_sdk/presentation/utils/app_fonts.dart';
+import 'package:survey_sdk/presentation/utils/colors.dart';
+import 'package:survey_sdk/presentation/utils/constants/constants.dart';
+
+class OptionCustomizeItem extends StatefulWidget {
+  final List<String> options;
+
+  const OptionCustomizeItem({Key? key, required this.options})
+      : super(key: key);
+
+  @override
+  State<OptionCustomizeItem> createState() => _OptionCustomizeItemState();
+}
+
+class _OptionCustomizeItemState extends State<OptionCustomizeItem> {
+  TextEditingController controller = TextEditingController();
+
+  void onEditingComplete() {
+    if (controller.text.isNotEmpty) {
+      setState(() {
+        widget.options.add(controller.text);
+      });
+    }
+    controller.clear();
+  }
+
+  void delete(int index) => setState(() => widget.options.removeAt(index));
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: widget.options.length,
+          itemBuilder: (context, index) => _Option(
+            option: widget.options[index],
+            delete: () => delete(index),
+          ),
+        ),
+        Row(
+          children: [
+            const Icon(
+              Icons.fiber_manual_record,
+              size: AppDimensions.sizeS,
+              color: AppColors.iconGrey,
+            ),
+            const SizedBox(
+              width: AppDimensions.margin2XS,
+            ),
+            Expanded(
+              child: TextFormField(
+                controller: controller,
+                onEditingComplete: onEditingComplete,
+                decoration: const InputDecoration.collapsed(
+                  hintText: 'Type new option here',
+                  hintStyle: TextStyle(
+                    color: AppColors.textLightGrey,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _Option extends StatelessWidget {
+  final String option;
+  final VoidCallback delete;
+
+  const _Option({
+    Key? key,
+    required this.option,
+    required this.delete,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom: AppDimensions.marginS,
+      ),
+      child: Row(
+        children: [
+          const Icon(
+            Icons.fiber_manual_record,
+            size: AppDimensions.sizeS,
+          ),
+          const SizedBox(
+            width: AppDimensions.margin2XS,
+          ),
+          Expanded(
+            child: Text(
+              option,
+              style: const TextStyle(fontSize: AppFonts.sizeL),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.close,
+              size: AppDimensions.sizeM,
+            ),
+            onPressed: delete,
+          ),
+        ],
+      ),
+    );
+  }
+}
