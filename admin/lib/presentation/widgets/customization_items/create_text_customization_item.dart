df15ce429f401ba270b6c9d15c54775baf99f9cf
Максим <91316352+maksnevor@users.noreply.github.com>
2023-02-17 17:35:56 +0300

    create text customization item (#16)
    * create text customization item
    
    * fix create text customization item
    
    ---------
    
    Co-authored-by: Maksim Nevar <syprenevor@mail.ru>

---
+++ admin/lib/presentation/widgets/customization_items/create_text_customization_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:survey_sdk/presentation/utils/app_fonts.dart';
+import 'package:survey_sdk/presentation/utils/colors.dart';
+import 'package:survey_sdk/presentation/utils/constants/constants.dart';
+
+class CreateTextCustomizationItem extends StatelessWidget {
+  final String title;
+  final void Function(String text) onChanged;
+
+  const CreateTextCustomizationItem({
+    Key? key,
+    required this.title,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(AppDimensions.marginM),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: AppFonts.sizeL,
+              fontWeight: AppFonts.weightSemiBold,
+              color: AppColors.black,
+            ),
+          ),
+          const SizedBox(height: AppDimensions.margin2XS),
+          Expanded(
+            child: TextField(
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Enter text',
+              ),
+              maxLines: 10,
+              style: const TextStyle(
+                fontSize: AppFonts.sizeL,
+                fontWeight: AppFonts.weightRegular,
+                color: AppColors.black,
+              ),
+              onChanged: onChanged,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
