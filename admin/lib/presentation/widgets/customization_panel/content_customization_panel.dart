32d33beb00ced628fbb10f1ee285360abe5708cc
Maksim Nevar <syprenevor@mail.ru>
2023-02-20 02:31:34 +0300

    Create customization panel for SliderQuestion

---
+++ admin/lib/presentation/widgets/customization_panel/content_customization_panel.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:survey_admin/presentation/utils/constants/constants.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/create_text_customization_item.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/customization_items_container.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/divisions_customization_item.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/min_max_customization_item.dart';
+
+class ContentCustomizationPanel extends StatelessWidget {
+  final ValueChanged<String> onTitleChanged;
+  final ValueChanged<String> onSubtitleChanged;
+  final void Function(int? min, int? max) onMinMaxChanged;
+  final ValueChanged<int?> onDivisionsChanged;
+
+  const ContentCustomizationPanel({
+    super.key,
+    required this.onDivisionsChanged,
+    required this.onMinMaxChanged,
+    required this.onSubtitleChanged,
+    required this.onTitleChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomizationItemsContainer(
+          title: 'Title',
+          children: [
+            CreateTextCustomizationItem(
+              maxHeight: AppDimensions.sizeL,
+              onChanged: onTitleChanged,
+            )
+          ],
+        ),
+        CustomizationItemsContainer(
+          title: 'SubTitle',
+          children: [
+            CreateTextCustomizationItem(
+              maxHeight: AppDimensions.sizeL,
+              onChanged: onSubtitleChanged,
+            ),
+          ],
+        ),
+        CustomizationItemsContainer(
+          title: 'Value',
+          children: [
+            MinMaxCustomizationItem(
+              onChanged: onMinMaxChanged,
+            ),
+          ],
+        ),
+        CustomizationItemsContainer(
+          title: 'Divisions',
+          children: [
+            DivisionsCustomizationItem(
+              onChanged: onDivisionsChanged,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
---
+++ admin/lib/presentation/widgets/customization_panel/slider_customization_panel.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:survey_admin/presentation/utils/app_fonts.dart';
+import 'package:survey_admin/presentation/utils/colors.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/color_customization_item.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/customization_items_container.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/thickness_customization_item.dart';
+
+class SliderCustomizationPanel extends StatelessWidget {
+  final ValueChanged<String?> onThicknessChanged;
+  final ValueChanged<Color> onActiveColorChanged;
+  final ValueChanged<Color> onInactiveColorChanged;
+  final ValueChanged<Color> onThumbColorChanged;
+  final ValueChanged<String?> onThumbChanged;
+
+  const SliderCustomizationPanel({
+    super.key,
+    required this.onActiveColorChanged,
+    required this.onInactiveColorChanged,
+    required this.onThicknessChanged,
+    required this.onThumbChanged,
+    required this.onThumbColorChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomizationItemsContainer(
+          title: 'Thickness',
+          children: [
+            ThicknessCustomizationItem(
+              onThicknessChanged: onThicknessChanged,
+              initialSize: AppFonts.sizeL,
+            ),
+          ],
+        ),
+        CustomizationItemsContainer(
+          title: 'Active',
+          children: [
+            ColorCustomizationItem(
+              initialColor: AppColors.switchBackgroundActive,
+              onColorPicked: onActiveColorChanged,
+            )
+          ],
+        ),
+        CustomizationItemsContainer(
+          title: 'Inactive',
+          children: [
+            ColorCustomizationItem(
+              initialColor: AppColors.switchBackgroundInactive,
+              onColorPicked: onInactiveColorChanged,
+            )
+          ],
+        ),
+        CustomizationItemsContainer(
+          title: 'Thumb',
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: ColorCustomizationItem(
+                    initialColor: AppColors.switchBackgroundActive,
+                    onColorPicked: onThumbColorChanged,
+                  ),
+                ),
+                Expanded(
+                  child: ThicknessCustomizationItem(
+                    onThicknessChanged: onThumbChanged,
+                    initialSize: AppFonts.sizeL,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
