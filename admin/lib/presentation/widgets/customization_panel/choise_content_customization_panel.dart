60d7d3a699a12ac0217944577d6ae00213e67999
Katya_Sobolevskaya <katyasobolevskaya11@gmail.com>
2023-02-21 19:15:00 +0300

    choice-question-customization-panel/ fixes

---
+++ admin/lib/presentation/widgets/customization_panel/choise_content_customization_panel.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:survey_admin/presentation/utils/constants/constants.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/create_text_customization_item.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/customization_items_container.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/option_customization_item.dart';
+
+class ChoiceQuestionCustomizationPanel extends StatelessWidget {
+  final ValueChanged<String> onTitleChanged;
+  final ValueChanged<String> onSubTitleChanged;
+  final ValueChanged<List<String>> onOptionsChanged;
+
+  const ChoiceQuestionCustomizationPanel({
+    Key? key,
+    required this.onTitleChanged,
+    required this.onSubTitleChanged,
+    required this.onOptionsChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomizationItemsContainer(
+          title: 'Title',
+          children: [
+            CreateTextCustomizationItem(
+              maxHeight: AppDimensions.sizeL,
+              onChanged: onTitleChanged,
+            )
+          ],
+        ),
+        CustomizationItemsContainer(
+          title: 'SubTitle',
+          children: [
+            CreateTextCustomizationItem(
+              maxHeight: AppDimensions.sizeL,
+              onChanged: onSubTitleChanged,
+            ),
+          ],
+        ),
+        CustomizationItemsContainer(
+          title: 'Options',
+          children: [
+            OptionCustomizationItem(
+              options: const [],
+              onChanged: onOptionsChanged,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
