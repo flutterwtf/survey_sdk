d94261e4e1e52189b3411eab16f4f1e1d946c2f9
Gleb Mihalkov <61373729+luffan@users.noreply.github.com>
2023-02-22 09:33:42 +0300

    Add themes for data classes, refactor widget pages, rework data to widget util (#26)

---
+++ core/lib/src/domain/entities/themes/input_question_theme.dart
@@ -0,0 +1,104 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:survey_core/src/domain/entities/api_object.dart';
+import 'package:survey_core/src/presentation/utils/app_fonts.dart';
+import 'package:survey_core/src/presentation/utils/colors.dart';
+
+class InputQuestionTheme extends ThemeExtension<InputQuestionTheme> with ApiObject {
+  final Color backgroundColor;
+  final Color borderColor;
+  final double borderWidth;
+  final Color hintColor;
+  final double hintSize;
+  final Color textColor;
+  final double textSize;
+  final int? minLines;
+  final int? maxLines;
+  final double? verticalPadding;
+  final double? horizontalPadding;
+
+  const InputQuestionTheme({
+    required this.backgroundColor,
+    required this.borderColor,
+    required this.borderWidth,
+    required this.hintColor,
+    required this.hintSize,
+    required this.textColor,
+    required this.textSize,
+    this.minLines,
+    this.maxLines,
+    this.verticalPadding,
+    this.horizontalPadding,
+  });
+
+  const InputQuestionTheme.common()
+      : this(
+          backgroundColor: Colors.white,
+          borderColor: Colors.black,
+          borderWidth: 1,
+          hintColor: AppColors.textLightGrey,
+          hintSize: AppFonts.sizeL,
+          textColor: AppColors.black,
+          textSize: AppFonts.sizeL,
+        );
+
+  @override
+  ThemeExtension<InputQuestionTheme> copyWith({
+    Color? backgroundColor,
+    Color? borderColor,
+    double? borderWidth,
+    Color? hintColor,
+    double? hintSize,
+    Color? textColor,
+    double? textSize,
+    int? minLines,
+    int? maxLines,
+    double? verticalPadding,
+    double? horizontalPadding,
+    String? hintText,
+  }) {
+    return InputQuestionTheme(
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      borderColor: borderColor ?? this.borderColor,
+      borderWidth: borderWidth ?? this.borderWidth,
+      hintColor: hintColor ?? this.hintColor,
+      hintSize: hintSize ?? this.hintSize,
+      textColor: textColor ?? this.textColor,
+      textSize: textSize ?? this.textSize,
+      minLines: minLines ?? this.minLines,
+      maxLines: maxLines ?? this.maxLines,
+      verticalPadding: verticalPadding ?? this.verticalPadding,
+      horizontalPadding: horizontalPadding ?? this.horizontalPadding,
+    );
+  }
+
+  @override
+  ThemeExtension<InputQuestionTheme> lerp(
+    covariant ThemeExtension<InputQuestionTheme>? other,
+    double t,
+  ) {
+    if (other is! InputQuestionTheme) {
+      return this;
+    }
+    return InputQuestionTheme(
+      backgroundColor: Color.lerp(backgroundColor, other.backgroundColor, t)!,
+      borderColor: Color.lerp(borderColor, other.borderColor, t)!,
+      borderWidth: lerpDouble(borderWidth, other.borderWidth, t)!,
+      hintColor: Color.lerp(hintColor, other.hintColor, t)!,
+      hintSize: lerpDouble(hintSize, other.hintSize, t)!,
+      textColor: Color.lerp(textColor, other.textColor, t)!,
+      textSize: lerpDouble(textSize, other.textSize, t)!,
+      minLines: lerpDouble(minLines, other.minLines, t)?.toInt(),
+      maxLines: lerpDouble(maxLines, other.maxLines, t)?.toInt(),
+      verticalPadding: lerpDouble(verticalPadding, other.verticalPadding, t)!,
+      horizontalPadding: lerpDouble(horizontalPadding, other.horizontalPadding, t)!,
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
+}
--- core/lib/src/presentation/utils/data_to_widget_util.dart
+++ core/lib/src/presentation/utils/data_to_widget_util.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:survey_core/src/domain/entities/question_types/choice_question_data.dart';
+import 'package:survey_core/src/domain/entities/question_types/input_question_data.dart';
+import 'package:survey_core/src/domain/entities/question_types/intro_question_data.dart';
 import 'package:survey_core/src/domain/entities/question_types/question_data.dart';
 import 'package:survey_core/src/domain/entities/question_types/slider_question_data.dart';
+import 'package:survey_core/src/presentation/choice_question/choice_question_page.dart';
+import 'package:survey_core/src/presentation/input_question/input_question_page.dart';
+import 'package:survey_core/src/presentation/intro_question/intro_question_page.dart';
 import 'package:survey_core/src/presentation/slider_question/slider_question_page.dart';
 
 //TODO: dynamic to survey result
@@ -9,8 +15,28 @@ typedef OnSendCallback = void Function(dynamic data);
 abstract class DataToWidgetUtil {
   static Widget createWidget(QuestionData data, OnSendCallback onSend) {
     switch (data.runtimeType) {
-      case SliderQuestionData: return SliderQuestionPage(data: data as SliderQuestionData, onSend: onSend);
-      default: throw Exception('Unimplemented error');
+      case SliderQuestionData:
+        return SliderQuestionPage(
+          data: data as SliderQuestionData,
+          onSend: onSend,
+        );
+      case ChoiceQuestionData:
+        return ChoiceQuestionPage(
+          data: data as ChoiceQuestionData,
+          onSend: onSend,
+        );
+      case InputQuestionData:
+        return InputQuestionPage(
+          data: data as InputQuestionData,
+          onSend: onSend,
+        );
+      case IntroQuestionData:
+        return IntroQuestionPage(
+          data: data as IntroQuestionData,
+          onSend: onSend,
+        );
+      default:
+        throw Exception('Unimplemented error');
     }
   }
 }
