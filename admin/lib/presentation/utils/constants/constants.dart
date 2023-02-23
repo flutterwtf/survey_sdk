92692ff20b220c68ade3c3e1d094c90c1d7234bf
AlexanderSimakov <50450591+AlexanderSimakov@users.noreply.github.com>
2023-02-23 10:32:26 +0300

    Export floating window (#17)
    * export-floating-window/add export floating window
    
    * export-floating-window/remove unused async keyword
    
    * export-floating-window/adjust colors
    
    * export-floating-window/hide public variables
    
    * export-floating-window/minor fixes

--- admin/lib/presentation/utils/constants/constants.dart
+++ admin/lib/presentation/utils/constants/constants.dart
@@ -13,6 +13,7 @@ abstract class AppDimensions {
   static const double size2M = 28.0;
   static const double size3M = 28.0;
   static const double sizeL = 48.0;
+  static const double size2XL = 96.0;
 
   static const double circularRadiusXS = 6.0;
   static const double circularRadiusS = 10.0;
