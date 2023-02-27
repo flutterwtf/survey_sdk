43817b54c5ce294fa1b5746f2192713d0e0fc3b5
Максим <91316352+maksnevor@users.noreply.github.com>
2023-02-27 18:00:16 +0300

    update onChange (#72)
    * update onChange
    
    * add validate for null
    
    ---------
    
    Co-authored-by: Maksim Nevar <syprenevor@mail.ru>

--- admin/lib/presentation/widgets/customization_items/secondary_button_customization_item.dart
+++ admin/lib/presentation/widgets/customization_items/secondary_button_customization_item.dart
@@ -18,12 +18,10 @@ class SecondaryButtonCustomizationItem extends StatefulWidget {
   });
 
   @override
-  State<SecondaryButtonCustomizationItem> createState() =>
-      _SecondaryButtonCustomizationItemState();
+  State<SecondaryButtonCustomizationItem> createState() => _SecondaryButtonCustomizationItemState();
 }
 
-class _SecondaryButtonCustomizationItemState
-    extends State<SecondaryButtonCustomizationItem> {
+class _SecondaryButtonCustomizationItemState extends State<SecondaryButtonCustomizationItem> {
   late bool _isShown;
   late String _text;
 
@@ -57,7 +55,14 @@ class _SecondaryButtonCustomizationItemState
                   ),
                   child: CustomizationTextField(
                     initialValue: widget.initialText,
-                    onChanged: (text) => widget.onChanged(_isShown, _text),
+                    onChanged: (text) {
+                      if (text != null) {
+                        setState(() {
+                          _text = text;
+                        });
+                      }
+                      widget.onChanged(_isShown, _text);
+                    },
                     decoration: InputDecoration(
                       hintText: context.localization.enter_text,
                       isCollapsed: true,
