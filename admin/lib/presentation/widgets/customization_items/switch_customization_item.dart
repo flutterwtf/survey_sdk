7cadfcb280ed90e87cdba3d35bdc2f7cae02ef7c
Maksim Nevar <syprenevor@mail.ru>
2023-03-02 19:04:33 +0300

    changed maximum length

--- admin/lib/presentation/widgets/customization_items/switch_customization_item.dart
+++ admin/lib/presentation/widgets/customization_items/switch_customization_item.dart
@@ -66,7 +66,9 @@ class _CustomSwitchState extends State<_CustomSwitch> {
         height: 18,
         width: 34,
         decoration: BoxDecoration(
-          color: _isToggled ? AppColors.switchBackgroundActive : AppColors.switchBackgroundInactive,
+          color: _isToggled
+              ? AppColors.switchBackgroundActive
+              : AppColors.switchBackgroundInactive,
           borderRadius: BorderRadius.circular(9),
         ),
         duration: AppDuration.customizationItemAnimation,
