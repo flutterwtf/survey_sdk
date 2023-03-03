0de388b8438312fb65ccd88cad686837d00f3496
Maksim Nevar <syprenevor@mail.ru>
2023-03-03 15:38:25 +0300

    added icons for question_list_item

--- admin/lib/presentation/widgets/builder_page/question_list_item.dart
+++ admin/lib/presentation/widgets/builder_page/question_list_item.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:survey_admin/presentation/app/localization/localizations.dart';
 import 'package:survey_admin/presentation/utils/app_fonts.dart';
+import 'package:survey_admin/presentation/utils/asset_strings.dart';
 import 'package:survey_admin/presentation/utils/colors.dart';
 import 'package:survey_admin/presentation/utils/constants/constants.dart';
+import 'package:survey_admin/presentation/widgets/vector_image.dart';
 import 'package:survey_core/survey_core.dart';
 
 class QuestionListItem extends StatelessWidget {
@@ -11,6 +14,23 @@ class QuestionListItem extends StatelessWidget {
     required this.questionData,
     super.key,
   });
+
+  Widget _questionImage(QuestionData questionData) {
+    switch (questionData.type) {
+      case 'Intro':
+        return const VectorImage(assetName: AssetStrings.intro);
+      case 'Input':
+        return const VectorImage(assetName: AssetStrings.input);
+      case 'Slider':
+        return const VectorImage(assetName: AssetStrings.slider);
+      case 'Choice':
+        return (questionData as ChoiceQuestionData).isMultipleChoice
+            ? const VectorImage(assetName: AssetStrings.multipleChoice)
+            : const VectorImage(assetName: AssetStrings.singleChoice);
+      default:
+        throw Exception('Unimplemented error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +59,7 @@ class QuestionListItem extends StatelessWidget {
               //TODO: to const maybe???
               height: 40,
               width: 40,
+              child: _questionImage(questionData),
             ),
             const SizedBox(width: AppDimensions.marginXS),
             Text(questionData.type),
