d649cb0eb53870958f9ada13213b12f257f8026d
Max <maksimk301190@gmail.com>
2023-03-07 14:29:15 +0300

    merge dev

--- admin/lib/presentation/widgets/customization_items/switch_customization_item.dart
+++ admin/lib/presentation/widgets/customization_items/switch_customization_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:survey_admin/presentation/utils/colors.dart';
 import 'package:survey_admin/presentation/utils/constants/app_durations.dart';
-import 'package:survey_admin/presentation/widgets/customization_items/customization_widgets/customization_text.dart';
+import 'package:survey_admin/presentation/widgets/customization_items/customization_items_container.dart';
 
 class SwitchCustomizationItem extends StatelessWidget {
   final String title;
