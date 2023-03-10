ff7d4ae773318515666bea7c92b08afee6940847
Vlad Kasenok <99754080+topaffiliate@users.noreply.github.com>
2023-03-10 08:28:33 +0100

    ci-cd-seting-up (#49)
    * ci-cde-seting-up/initial commit
    
    * ci-cd-setting-up/mistake correction
    
    * ci-cd-setting-up/commands correction
    
    * ci-cd-setting-up/scripts added
    
    * ci-cd-setting-up/test coverage added
    
    * ci-cd-setting-up/admin panel added to pages
    
    * ci-cd-setting-up/survey cubit test + threads resolving
    
    * ci-cd-setting-up/threads resolving
    
    * ci-cd/pull dev
    
    ---------
    
    Co-authored-by: losich.maksim <losych.maksim@gmail.com>

---
+++ scripts/coverage_check.sh
@@ -0,0 +1,13 @@
+min_coverage=3
+
+coverage_check=$(flutter pub run test_cov_console --pass=$min_coverage)
+
+if [ "$coverage_check" == "PASSED" ]
+then
+  echo "Good coverage"
+  exit 0
+# else exit 1
+else
+  echo "Coverage less than $min_coverage"
+  exit 1
+fi
