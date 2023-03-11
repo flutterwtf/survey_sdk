ea536b2857ba2ee8cd52c5bcbbeb78ed31e3a4cc
Максим <91316352+maksnevor@users.noreply.github.com>
2023-03-11 10:39:06 +0300

    refactored style for multiswitch (#125)

--- admin/lib/presentation/widgets/customization_panel/input/input_customization_tab.dart
+++ admin/lib/presentation/widgets/customization_panel/input/input_customization_tab.dart
@@ -49,9 +49,6 @@ class InputCustomizationTab extends CustomizationTab {
       children: [
         CustomizationItemsContainer(
           isTopDividerShown: true,
-          itemsPadding: const EdgeInsets.all(
-            AppDimensions.marginM,
-          ),
           children: [
             MultilineSwitch(
               onChanged: onMultilineChanged,
