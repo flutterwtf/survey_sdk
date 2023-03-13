4bb4840269aef5e11e4a9b0415c69067ece244c2
tokyolem <heenssss@mail.ru>
2023-03-13 21:22:50 +0300

    refactor-localization/localization update

--- admin/lib/presentation/pages/new_question_page/new_question_page.dart
+++ admin/lib/presentation/pages/new_question_page/new_question_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:survey_admin/presentation/app/localization/localizations.dart';
 import 'package:survey_admin/presentation/pages/new_question_page/new_question_tabs.dart';
 import 'package:survey_admin/presentation/utils/app_fonts.dart';
 import 'package:survey_admin/presentation/utils/colors.dart';
@@ -6,9 +7,6 @@ import 'package:survey_admin/presentation/utils/constants/app_assets.dart';
 import 'package:survey_admin/presentation/utils/constants/constants.dart';
 import 'package:survey_admin/presentation/utils/theme_extension.dart';
 import 'package:survey_admin/presentation/widgets/vector_image.dart';
-
-// TODO(dev): check localization
-const _title = 'New screen';
 
 class NewQuestionPage extends StatefulWidget {
   const NewQuestionPage({super.key});
@@ -111,7 +109,7 @@ class _AppBarTitle extends StatelessWidget {
     return Align(
       alignment: Alignment.centerLeft,
       child: Text(
-        _title,
+        context.localization.new_screen,
         style: context.theme.textTheme.labelLarge?.copyWith(
           fontWeight: AppFonts.weightRegular,
         ),
