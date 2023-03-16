dcbfe34895bb981cbb2c1c0acd1c68992493d846
@akarmazin <akarmazin7210@gmail.com>
2023-03-16 15:32:15 +0300

    di fix

--- admin/lib/presentation/app/di/injector.dart
+++ admin/lib/presentation/app/di/injector.dart
@@ -1,4 +1,6 @@
 import 'package:get_it/get_it.dart';
+import 'package:survey_admin/data/data_sources/filesystem_data_source.dart';
+import 'package:survey_admin/data/repositories/survey_data_repository.dart';
 import 'package:survey_admin/presentation/app/app_cubit.dart';
 import 'package:survey_admin/presentation/pages/builder/builder_cubit.dart';
 import 'package:survey_admin/presentation/pages/new_question_page/new_question_cubit.dart';
@@ -20,4 +22,10 @@ void _initCubits() {
     ..registerFactory<NewQuestionCubit>(NewQuestionCubit.new);
 }
 
-void _initRepositories() {}
+
+
+
+void _initRepositories() {
+  final filesystemDataSource = FilesystemDataSourceImpl();
+  SurveyDataRepositoryImpl(filesystemDataSource);
+}
