0481b243cad6eb5828909f80fa7b78a2c4ce186e
@akarmazin <akarmazin7210@gmail.com>
2023-03-17 10:51:37 +0300

    injector fix

--- admin/lib/presentation/app/di/injector.dart
+++ admin/lib/presentation/app/di/injector.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:survey_admin/data/data_sources/filesystem_data_source.dart';
+import 'package:survey_admin/data/data_sources/interfaces/filesystem_data_source.dart';
 import 'package:survey_admin/data/repositories/survey_data_repository.dart';
+import 'package:survey_admin/domain/repository_interfaces/survey_data_repository.dart';
 import 'package:survey_admin/presentation/app/app_cubit.dart';
 import 'package:survey_admin/presentation/pages/builder/builder_cubit.dart';
 import 'package:survey_admin/presentation/pages/new_question_page/new_question_cubit.dart';
@@ -13,7 +15,11 @@ Future<void> initInjector() async {
   _initCubits();
 }
 
-Future<void> _initDataSources() async {}
+Future<void> _initDataSources() async {
+  i.registerSingleton<FilesystemDataSource>(
+    FilesystemDataSourceImpl(),
+  );
+}
 
 void _initCubits() {
   i
@@ -22,10 +28,8 @@ void _initCubits() {
     ..registerFactory<NewQuestionCubit>(NewQuestionCubit.new);
 }
 
-
-
-
 void _initRepositories() {
-  final filesystemDataSource = FilesystemDataSourceImpl();
-  SurveyDataRepositoryImpl(filesystemDataSource);
+  i.registerSingleton<SurveyDataRepository>(
+    SurveyDataRepositoryImpl(i.get<FilesystemDataSource>()),
+  );
 }
