c33166ddfa6ba4349d5dd889da58aee815dc30d1
itskarmo <113526559+itskarmo@users.noreply.github.com>
2023-03-20 19:52:00 +0300

    tests refactoring + filesystem_data_source edit for web (#149)
    Co-authored-by: Max <maksimk301190@gmail.com>
    Co-authored-by: Max <124910445+maksim-work@users.noreply.github.com>
    Co-authored-by: @akarmazin <akarmazin7210@gmail.com>
    Co-authored-by: Alesia <alesya.r.03@gmail.com>

--- admin/lib/data/repositories/session_storage_repository_impl.dart
+++ admin/lib/data/repositories/session_storage_repository_impl.dart
@@ -1,4 +1,4 @@
-import 'package:survey_admin/data/interfaces/session_storage_data_source.dart';
+import 'package:survey_admin/data/data_sources/interfaces/session_storage_data_source.dart';
 import 'package:survey_admin/domain/repository_interfaces/session_storage_repository.dart';
 import 'package:survey_core/survey_core.dart';
 
--- admin/lib/presentation/pages/builder/builder_page.dart
+++ admin/lib/presentation/pages/builder/builder_page.dart
@@ -157,6 +157,7 @@ class _PreviewTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cubit = BlocProvider.of<BuilderCubit>(context);
     return Padding(
       padding: const EdgeInsets.only(
         top: AppDimensions.margin2XS,
@@ -167,7 +168,7 @@ class _PreviewTab extends StatelessWidget {
         onPressed: () {
           showExportFloatingWindow(
             context,
-            onDownloadPressed: () {},
+            onDownloadPressed: cubit.downloadExportedQuestions,
             onCopyPressed: () {},
           );
         },
