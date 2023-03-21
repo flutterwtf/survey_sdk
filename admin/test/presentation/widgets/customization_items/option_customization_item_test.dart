cddf9fe783aea3a2215dba7f089accaccd95e440
Maksim Nevar <syprenevor@mail.ru>
2023-03-21 12:26:35 +0300

    fix test for option customization item

--- admin/test/presentation/widgets/customization_items/option_customization_item_test.dart
+++ admin/test/presentation/widgets/customization_items/option_customization_item_test.dart
@@ -10,12 +10,17 @@ void main() {
     testWidgets('OptionCustomizationItem adds new option when text is entered',
         (WidgetTester tester) async {
       var options = <String>['Option 1', 'Option 2'];
+      var ruleValue = 0;
       await tester.pumpWidget(
         AppTester(
           child: OptionCustomizationItem(
             options: options,
+            ruleValue: ruleValue,
             onChanged: (newOptions) {
               options = newOptions;
+            },
+            onRuleLimitedChanged: (newRuleValue) {
+              ruleValue = newRuleValue;
             },
           ),
         ),
@@ -36,12 +41,17 @@ void main() {
         'OptionCustomizationItem deletes option when delete button is pressed',
         (WidgetTester tester) async {
       var options = <String>['Option 1', 'Option 2', 'Option 3'];
+      var ruleValue = 0;
       await tester.pumpWidget(
         AppTester(
           child: OptionCustomizationItem(
             options: options,
+            ruleValue: ruleValue,
             onChanged: (newOptions) {
               options = newOptions;
+            },
+            onRuleLimitedChanged: (newRuleValue) {
+              ruleValue = newRuleValue;
             },
           ),
         ),
@@ -57,6 +67,35 @@ void main() {
       expect(options.length, 2);
       expect(find.text('Option 1'), findsOneWidget);
       expect(find.text('Option 2'), findsOneWidget);
+      expect(find.text('Option 3'), findsNothing);
+    });
+
+    testWidgets(
+        'Delete option and change ruleValue when ruleValue = last value',
+        (WidgetTester tester) async {
+      var options = <String>['Option 1', 'Option 2', 'Option 3'];
+      var ruleValue = 3;
+      await tester.pumpWidget(
+        AppTester(
+          child: OptionCustomizationItem(
+            options: options,
+            ruleValue: ruleValue,
+            onChanged: (newOptions) {
+              options = newOptions;
+            },
+            onRuleLimitedChanged: (newRuleValue) {
+              ruleValue = newRuleValue;
+            },
+          ),
+        ),
+      );
+
+      expect(find.text('Option 3'), findsOneWidget);
+
+      await tester.tap(find.widgetWithIcon(IconButton, Icons.close).last);
+      await tester.pump();
+
+      expect(ruleValue, 0);
       expect(find.text('Option 3'), findsNothing);
     });
   });
