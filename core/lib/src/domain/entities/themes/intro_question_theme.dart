c87c4a7229dd69d9c5cf18e0642cd827e46589e0
Alesia <alesya.r.03@gmail.com>
2023-03-24 16:13:01 +0300

    import utils

--- core/lib/src/domain/entities/themes/intro_question_theme.dart
+++ core/lib/src/domain/entities/themes/intro_question_theme.dart
@@ -3,9 +3,7 @@ import 'dart:ui';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:survey_core/src/domain/entities/api_object.dart';
-import 'package:survey_core/src/presentation/utils/app_colors.dart';
-import 'package:survey_core/src/presentation/utils/app_dimensions.dart';
-import 'package:survey_core/src/presentation/utils/app_fonts.dart';
+import 'package:survey_core/src/presentation/utils/utils.dart';
 
 class IntroQuestionTheme extends ThemeExtension<IntroQuestionTheme>
     with ApiObject, EquatableMixin {
--- core/lib/src/presentation/utils/utils.dart
+++ core/lib/src/presentation/utils/utils.dart
@@ -6,4 +6,4 @@ export 'package:survey_core/src/presentation/utils/app_dimensions.dart';
 export 'package:survey_core/src/presentation/utils/app_durations.dart';
 export 'package:survey_core/src/presentation/utils/app_fonts.dart';
 export 'package:survey_core/src/presentation/utils/data_to_widget_util.dart';
-export 'package:survey_core/src/presentation/utils/theme_extension.dart';
+export 'package:survey_core/src/presentation/utils/theme_data_extension.dart';
