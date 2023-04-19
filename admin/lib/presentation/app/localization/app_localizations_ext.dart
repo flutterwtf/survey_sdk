e8bc17b72652fbcec468f22a71ff0a2b50d61430
maksnevor <maksnevor14@gmail.com>
2023-04-19 13:30:47 +0300

    delete todo for this task

--- admin/lib/presentation/app/localization/app_localizations_ext.dart
+++ admin/lib/presentation/app/localization/app_localizations_ext.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-// TODO(dev): write test for checking the point above.
 extension AppLocalizationsExt on BuildContext {
   AppLocalizations get localization => AppLocalizations.of(this);
 }
