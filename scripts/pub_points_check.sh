2e66cfa011227ba746228be6e3d22370d10aed92
@akarmazin <akarmazin7210@gmail.com>
2023-05-18 20:22:31 +0300

    Continuously achieve the highest pana score: CI/CD test 4

--- scripts/pub_points_check.sh
+++ scripts/pub_points_check.sh
@@ -1,2 +1,12 @@
 threshold=50
 dart pub global activate pana
+pana --exit-code-threshold $threshold
+retVal=$?
+if [ $retVal -eq 0 ]
+then
+  echo "Good pana score"
+  exit 0
+else
+  echo "Pana score less than minimum"
+  exit 1
+fi
