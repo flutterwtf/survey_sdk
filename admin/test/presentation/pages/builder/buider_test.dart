13bc1811d1dec2425f8b993c09b8b1610dfcc02b
losich.maksim <losych.maksim@gmail.com>
2023-08-14 00:45:28 +0300

    common-theme: analyzer fix

--- admin/test/presentation/pages/builder/buider_test.dart
+++ admin/test/presentation/pages/builder/buider_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/mockito.dart';
