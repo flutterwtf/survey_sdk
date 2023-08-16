cd9e98713947f481897c1637cbbc21e314b26e74
itskarmo <113526559+itskarmo@users.noreply.github.com>
2023-08-16 03:56:34 -0400

    UI fixes V2 (#272)

--- admin/lib/presentation/widgets/base/customization_tab.dart
+++ admin/lib/presentation/widgets/base/customization_tab.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 abstract class CustomizationTab extends StatelessWidget {
   final String title;
+  final bool showToolTip;
 
   const CustomizationTab({
     required this.title,
+    this.showToolTip = false,
     super.key,
   });
 }
--- core/lib/src/presentation/utils/survey_durations.dart
+++ core/lib/src/presentation/utils/survey_durations.dart
@@ -2,6 +2,8 @@ class SurveyDurations {
   static const Duration questionBottomButton = Duration(milliseconds: 200);
   static const Duration customizationItemDuration = _iosAnimationDuration;
   static const Duration panelSwitchingDuration = _iosAnimationDuration;
+  static const Duration animateToSpecificPageDuration =
+  Duration(milliseconds: 500);
 
   static const Duration _iosAnimationDuration = Duration(milliseconds: 350);
 }
